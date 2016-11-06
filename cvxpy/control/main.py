11162fbdccfbc5049002a7254a63ba0fa2fc7224
AtsushiSakai <asakai.amsl+github@gmail.com>
2016-11-05 19:47:32 -0700

    first release

--- cvxpy/control/main.py
+++ cvxpy/control/main.py
@@ -5,10 +5,11 @@ Simple Model Predictive Control Simulation
 
 author Atsushi Sakai
 """
-
+import time
 from cvxpy import *
 import numpy as np
 import matplotlib.pyplot as plt
+print("Simulation start")
 
 np.random.seed(1)
 n = 4   #state size
@@ -36,31 +37,43 @@ for t in range(T):
 # sums problem objectives and concatenates constraints.
 prob = sum(states)
 prob.constraints += [x[:,T] == 0, x[:,0] == x_0]
-prob.solve()
+
+start = time.time()
+result=prob.solve(verbose=True)
+elapsed_time = time.time() - start
+print ("calc time:{0}".format(elapsed_time)) + "[sec]"
+
+if result == float("inf"):
+    print("Cannot optimize")
+    import sys
+    sys.exit()
+    #  return
 
 f = plt.figure()
 # Plot (u_t)_1.
 ax = f.add_subplot(211)
-plt.plot(u[0,:].value.A.flatten(),'-r',label="u1")
-plt.plot(u[1,:].value.A.flatten(),'-b',label="u2")
-plt.ylabel(r"$(u_t)$", fontsize=16)
+u1=np.array(u[0,:].value[0,:])[0].tolist()
+u2=np.array(u[1,:].value[0,:])[0].tolist()
+plt.plot(u1,'-r',label="u1")
+plt.plot(u2,'-b',label="u2")
+plt.ylabel(r"$u_t$", fontsize=16)
 plt.yticks(np.linspace(-1.0, 1.0, 3))
 plt.legend()
 plt.grid(True)
 
 # Plot (u_t)_2.
 plt.subplot(2,1,2)
-x1 = x[0,:].value.A.flatten()
-x2 = x[1,:].value.A.flatten()
-x3 = x[2,:].value.A.flatten()
-x4 = x[2,:].value.A.flatten()
-plt.plot(range(51), x1,'-r',label="x1")
-plt.plot(range(51), x2,'-b',label="x2")
-plt.plot(range(51), x3,'-g',label="x3")
-plt.plot(range(51), x4,'-k',label="x4")
+x1=np.array(x[0,:].value[0,:])[0].tolist()
+x2=np.array(x[1,:].value[0,:])[0].tolist()
+x3=np.array(x[2,:].value[0,:])[0].tolist()
+x4=np.array(x[3,:].value[0,:])[0].tolist()
+plt.plot(range(T+1), x1,'-r',label="x1")
+plt.plot(range(T+1), x2,'-b',label="x2")
+plt.plot(range(T+1), x3,'-g',label="x3")
+plt.plot(range(T+1), x4,'-k',label="x4")
 plt.yticks([-25, 0, 25])
 plt.ylim([-25, 25])
-plt.ylabel(r"$(x_t)$", fontsize=16)
+plt.ylabel(r"$x_t$", fontsize=16)
 plt.xlabel(r"$t$", fontsize=16)
 plt.grid(True)
 plt.legend()
