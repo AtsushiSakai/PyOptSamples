eb2f55ce21829432aea30894277d9000e33c72f0
AtsushiSakai <asakai.amsl+github@gmail.com>
2017-02-08 11:44:16 -0800

    first release

--- tests/test_pyecosqp.py
+++ tests/test_pyecosqp.py
@@ -13,6 +13,12 @@ class Test(unittest.TestCase):
     def test_1(self):
         pyecosqp.pyecosqp.test1()
 
+    def test_2(self):
+        pyecosqp.pyecosqp.test2()
+
+    def test_3(self):
+        pyecosqp.pyecosqp.test2()
+
 
 if __name__ == '__main__':
     unittest.main()
