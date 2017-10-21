e4fa549298c6c72100c808367730e8c061cdaaa7
Atsushi Sakai <asakai.amsl+github@gmail.com>
2017-10-20 22:56:34 -0700

    add minimal_cost_network_flow sample

--- OperationsResearch/minimal_cost_network_flow.py
+++ OperationsResearch/minimal_cost_network_flow.py
@@ -15,10 +15,10 @@ def main():
 
     ni = [1, 1, 2, 3, 3, 4, 4, 5]
     nj = [2, 3, 3, 4, 5, 1, 5, 2]
-    cij = [2, 5, 3, 1, 2, 0, 2, 4]
+    cij = np.array([4, 2, 1, 1, 2, 5, 2, 4])
     inf = 100000.0
     uij = [inf, inf, inf, inf, 1, inf, inf, inf]
-    bi = [-5, 10, 0, -2, -3]
+    bi = [-5, 11, -1, -2, -3]
 
     print("ni", ni)
     print("nj", nj)
@@ -27,19 +27,18 @@ def main():
     print("bi", bi)
 
     x = cvxpy.Variable(len(ni))
-    objective = cvxpy.Minimize(np.transpose(cij) * x)
+    objective = cvxpy.Minimize(cij.T * x)
     constraints = [0 <= x, x <= uij]
     for iin in range(1, len(bi) + 1):
-        inp = sum([x[i - 1] for i in ni if i == iin])
-        out = sum([x[i - 1] for i in nj if i == iin])
-        constraints += [(inp - out) == bi[iin - 1]]
+        inp = sum([x[i - 1] for i in range(1, len(ni)) if ni[i - 1] == iin])
+        out = sum([x[i - 1] for i in range(1, len(ni)) if nj[i - 1] == iin])
+        constraints += [inp - out == bi[iin - 1]]
 
     prob = cvxpy.Problem(objective, constraints)
 
-    result = prob.solve()
+    result = prob.solve(solver=cvxpy.ECOS)
     print("Opt result:", result)
-    print("optimal parameter:\n", x.value)
-    print("Lagrange parameter\n", constraints[0].dual_value)
+    print("optimal parameter:\n", [int(i) for i in x.value])
     print("status:" + prob.status)
 
 
